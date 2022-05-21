08b1b47d15d31b2ea230682a6748a60158c8a316
12whoisyou <Farouqjalabi@gmail.com>
2022-05-21 10:53:04 +0200

    Just the start, have to plan it in figma!

--- onetasker/components/footer.js
+++ onetasker/components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div class="flex justify-center border-solid border-t-2">
-      <footer class="">Made by Red Cube</footer>
+    <div className="flex justify-center border-solid border-t-2">
+      <footer className="">Made by Red Cube</footer>
     </div>
   );
 }
