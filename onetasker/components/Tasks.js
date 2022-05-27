9417b4c441eac16ea02c3b17b11a11e764f61a28
12whoisyou <Farouqjalabi@gmail.com>
2022-05-28 00:43:28 +0200

    Can add tasks with no bug

--- onetasker/components/Tasks.js
+++ onetasker/components/Tasks.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+//! Can take empty
+//! Too long don't
 export default function Tasks() {
   const fake_list = ["blabla", "What is max charecter?", "Hello World"];
 
@@ -23,17 +25,20 @@ export default function Tasks() {
         drop-shadow-[7px_7px_0_rgba(0,0,0,0.25)]
         text-2xl"
           onClick={() => {
-            console.log(tasks);
-            setTasks([...tasks, input]);
+            const prevTasks = tasks;
+            setTasks((tasks) => [input, ...tasks]);
           }}
         >
           +
         </button>
       </div>
 
-      <ol className="w-100 mx-auto list-decimal text-3xl">
-        {tasks.map((task) => (
-          <li key={tasks.length}>task</li>
+      <ol className="w-100 mx-auto text-3xl">
+        {tasks.map((task, id) => (
+          <li key={id} className="my-5 bg-white rounded-xl p-3 flex">
+            {task}
+            <div className="ml-auto pl-5">x</div>
+          </li>
         ))}
       </ol>
     </div>
